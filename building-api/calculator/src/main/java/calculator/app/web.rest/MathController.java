90665f8fe848c87c5e26d6e6156ec5fbaaf4d8c4
sunless13 <sunlesskelv@gmail.com>
2024-06-06 22:50:20 +0300

    cors policy error left on standby

--- building-api/calculator/src/main/java/calculator/app/web.rest/MathController.java
+++ building-api/calculator/src/main/java/calculator/app/web.rest/MathController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/arithmetic")
-@CrossOrigin(origins = "http://localhost:8080")
 public class MathController {
   MathService mathService = new MathService();
 
