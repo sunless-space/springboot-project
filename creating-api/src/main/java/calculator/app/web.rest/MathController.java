b7fe827d056b1ed71085ef5ece84354465601eb3
sunless13 <sunlesskelv@gmail.com>
2024-06-17 19:19:56 +0300

    fixed cors problem??

--- creating-api/src/main/java/calculator/app/web.rest/MathController.java
+++ creating-api/src/main/java/calculator/app/web.rest/MathController.java
@@ -3,12 +3,14 @@ package calculator.app.web.rest;
 import calculator.app.service.DTO.MathRequestDTO;
 import calculator.app.service.DTO.MathResponse;
 import calculator.app.service.MathService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/arithmetic")
 public class MathController {
   MathService mathService = new MathService();
